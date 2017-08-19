30eb2b6f5fedd8cb5ff07e054c6efb5a2b27f5d9
Bruna <Bruna@PC-BRUNA>
2017-08-19 03:19:50 -0300

    Inicia implementação de Loan

---
+++ Source/BenfeitorApi/Controllers/LoanController.cs
@@ -0,0 +1,49 @@
+﻿using BenfeitorApi.Models;
+using BenfeitorApi.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace BenfeitorApi.Controllers
+{
+    public class LoanController : ApiController
+    {
+
+        private ILoanService _loanService;
+
+        public LoanController(ILoanService loanService)
+        {
+
+            this._loanService = loanService;
+
+        }
+
+        [HttpPost]
+        [Route("loan")]
+        public IHttpActionResult CreateLoan(CreateLoanRequest request)
+        {
+
+            // TODO: Create loan
+            //_loanService.CreateLoan(
+
+
+            return this.GetLoan(new Guid());
+        }
+
+        [HttpGet]
+        [Route("loans/{id}")]
+        public IHttpActionResult GetLoan(Guid personKey)
+        {
+
+            var response = new PersonResponse()
+            {
+                PersonKey = personKey,
+                //Name = request.Name
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ Source/BenfeitorApi/Services/LoanService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BenfeitorApi.Models;
+using Repository.Interfaces;
+
+namespace BenfeitorApi.Services
+{
+    public class LoanService : ILoanService
+    {
+        private ILoanRepository _loanRepository;
+
+        public LoanService(ILoanRepository loanRepository)
+        {
+
+            this._loanRepository = loanRepository;
+
+        }
+
+        public LoanResponse CreateLoan(CreateLoanRequest request)
+        {
+            // TODO: Create loan
+
+            var id = "1";
+
+            return this.GetLoan(id);
+        }
+
+        public LoanResponse GetLoan(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
