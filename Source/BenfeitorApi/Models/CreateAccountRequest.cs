b87dbc25fc890934ea23b4fc00706b656bb6b1c2
Matheus André Riboli <matheusriboli@hotmail.com>
2017-08-19 00:54:07 -0300

    Adds basic structure.

---
+++ Source/BenfeitorApi/Models/CreateAccountRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BenfeitorApi.Models
+{
+    public class CreateAccountRequest
+    {
+
+        public string Name { get; set; }
+
+    }
+}
